79f3cc9e4542eb2d1edefb9000cb579c3d514f41
M6code <benjamynfrank101@gmail.com>
2020-05-08 15:03:27 +0100

    Updates

---
+++ precache-manifest.56c6ef2c888c673b0b2992ffb45a69e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d4069d45cfce831c636ceb240019ca",
+    "url": "/TaskManager-reactjs/index.html"
+  },
+  {
+    "revision": "49a6ce31c8c5ffd42e8d",
+    "url": "/TaskManager-reactjs/static/css/main.0e632dbf.chunk.css"
+  },
+  {
+    "revision": "b0600b3c940cfdbef6fb",
+    "url": "/TaskManager-reactjs/static/js/2.15ea54b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TaskManager-reactjs/static/js/2.15ea54b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a6ce31c8c5ffd42e8d",
+    "url": "/TaskManager-reactjs/static/js/main.8989d725.chunk.js"
+  },
+  {
+    "revision": "13134730de976871dbc9",
+    "url": "/TaskManager-reactjs/static/js/runtime-main.d3891c1a.js"
+  }
+]);
